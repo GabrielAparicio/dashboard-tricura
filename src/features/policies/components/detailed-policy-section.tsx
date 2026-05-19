28546f14ddd565d6484d9410a95c23ffe3d69fe5
Gabriel <sgaparicio22@gmail.com>
2026-05-19 01:08:47 -0500

    Add simple loading state for policy details

--- src/features/policies/components/detailed-policy-section.tsx
+++ src/features/policies/components/detailed-policy-section.tsx
@@ -1,9 +1,21 @@
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 import { ErrorBoundary } from 'react-error-boundary';
 import { Suspense } from 'react';
 import DetailedPolicy from './detailed-policy';
 
 interface DetailedPolicySectionProps {
   policyId: string;
+}
+
+function PolicySkeleton() {
+  return (
+    <Stack>
+      <Typography variant="h6" gutterBottom align="center">
+        Policy details loading...
+      </Typography>
+    </Stack>
+  );
 }
 
 export default function DetailedPolicySection({
@@ -13,7 +25,7 @@ export default function DetailedPolicySection({
     <ErrorBoundary
       fallback={<div>Something went wrong with policy details...</div>}
     >
-      <Suspense fallback={<div>Loading policy details...</div>}>
+      <Suspense fallback={<PolicySkeleton />}>
         <DetailedPolicy policyID={policyId} />
       </Suspense>
     </ErrorBoundary>
